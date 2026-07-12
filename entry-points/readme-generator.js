9a29a2b6636d997634d7d38ba282069c855eb2e1
z-apply <admin@zapply.jobs>
2026-07-12 13:50:18 -0500

    chore: remove unused entry-points/ (consumer repos are pure display — pipeline uses lib/readme-generator directly)

--- entry-points/readme-generator.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const { config, loadSibling } = require('./config-loader');
-const jobCategoriesPath = loadSibling('job_categories.json');
-const jobCategories = jobCategoriesPath ? require(jobCategoriesPath) : {};
-const { createReadmeGenerator } = require(path.join(__dirname, '../lib/readme-generator.js'));
-module.exports = createReadmeGenerator(config, jobCategories, process.cwd());
